59d84871c9807407aff3ab27eb8d93f7a54fc4ca
Bob Smith <rbrthsmth@gmail.com>
2018-08-29 15:22:29 -0400

    complete rework

---
+++ ebooks.py
@@ -0,0 +1,121 @@
+import random
+import re
+import sys
+import twitter
+import markov
+from html.entities import name2codepoint as n2c
+from urllib.request import urlopen
+from local_settings import *
+
+def connect(type='twitter'):
+    if type == 'twitter':
+        return twitter.Api(consumer_key=MY_CONSUMER_KEY,
+                       consumer_secret=MY_CONSUMER_SECRET,
+                       access_token_key=MY_ACCESS_TOKEN_KEY,
+                       access_token_secret=MY_ACCESS_TOKEN_SECRET,
+                       tweet_mode='extended')
+    return None
+
+
+def entity(text):
+    if text[:2] == "&#":
+        try:
+            if text[:3] == "&#x":
+                return chr(int(text[3:-1], 16))
+            else:
+                return chr(int(text[2:-1]))
+        except ValueError:
+            pass
+    else:
+        guess = text[1:-1]
+        if guess == "apos":
+            guess = "lsquo"
+        numero = n2c[guess]
+        try:
+            text = chr(numero)
+        except KeyError:
+            pass
+    return text
+
+
+def filter_status(text):
+    text = re.sub(r'\b(RT|MT) .+', '', text)  # take out anything after RT or MT
+    text = re.sub(r'(\#|@|(h\/t)|(http))\S+', '', text)  # Take out URLs, hashtags, hts, etc.
+    text = re.sub('\s+', ' ', text)  # collaspse consecutive whitespace to single spaces.
+    text = re.sub(r'\"|\(|\)', '', text)  # take out quotes.
+    text = re.sub(r'\s+\(?(via|says)\s@\w+\)?', '', text)  # remove attribution
+    text = re.sub(r'<[^>]*>','', text) #strip out html tags from mastodon posts
+    htmlsents = re.findall(r'&\w+;', text)
+    for item in htmlsents:
+        text = text.replace(item, entity(item))
+    text = re.sub(r'\xe9', 'e', text)  # take out accented e
+    return text
+
+if __name__ == "__main__":
+    order = ORDER
+    guess = 0
+    if ODDS and not DEBUG:
+        guess = random.randint(0, ODDS - 1)
+
+    if guess:
+        print(str(guess) + " No, sorry, not this time.")  # message if the random number fails.
+        sys.exit()
+    else:
+        api = connect()
+        source_statuses = []
+        if STATIC_TEST:
+            file = TEST_SOURCE
+            print(">>> Generating from {0}".format(file))
+            string_list = open(file).readlines()
+            for item in string_list:
+                source_statuses += item.split(",")
+        if len(source_statuses) == 0:
+            print("No statuses found!")
+            sys.exit()
+        mine = markov.MarkovChainer(order)
+        for status in source_statuses:
+            if not re.search('([\.\!\?\"\']$)', status):
+                status += "."
+            mine.add_text(status)
+        for x in range(0, 10):
+            ebook_status = mine.generate_sentence()
+
+        # randomly drop the last word, as Horse_ebooks appears to do.
+        if random.randint(0, 4) == 0 and re.search(r'(in|to|from|for|with|by|our|of|your|around|under|beyond)\s\w+$', ebook_status) is not None:
+            print("Losing last word randomly")
+            ebook_status = re.sub(r'\s\w+.$', '', ebook_status)
+            print(ebook_status)
+
+        # if a tweet is very short, this will randomly add a second sentence to it.
+        if ebook_status is not None and len(ebook_status) < 40:
+            rando = random.randint(0, 10)
+            if rando == 0 or rando == 7:
+                print("Short tweet. Adding another sentence randomly")
+                newer_status = mine.generate_sentence()
+                if newer_status is not None:
+                    ebook_status += " " + mine.generate_sentence()
+                else:
+                    ebook_status = ebook_status
+            elif rando == 1:
+                # say something crazy/prophetic in all caps
+                print("ALL THE THINGS")
+                ebook_status = ebook_status.upper()
+
+        # throw out tweets that match anything from the source account.
+        if ebook_status is not None and len(ebook_status) < 210:
+            for status in source_statuses:
+                if ebook_status[:-1] not in status:
+                    continue
+                else:
+                    print("TOO SIMILAR: " + ebook_status)
+                    sys.exit()
+
+            if not DEBUG:
+                if ENABLE_TWITTER_POSTING:
+                    status = api.PostUpdate(ebook_status)
+            print(ebook_status)
+
+        elif not ebook_status:
+            print("Status is empty, sorry.")
+        else:
+            print("TOO LONG: " + ebook_status)
---
+++ local_settings.py
@@ -0,0 +1,24 @@
+'''
+Local Settings for a heroku_ebooks account. 
+'''
+
+import os
+
+# Configuration for Twitter API
+ENABLE_TWITTER_POSTING = True # Tweet resulting status?
+MY_CONSUMER_KEY = os.environ.get('EBOOKS_CONSUMER_KEY')
+MY_CONSUMER_SECRET = os.environ.get('EBOOKS_CONSUMER_SECRET')
+MY_ACCESS_TOKEN_KEY = os.environ.get('EBOOKS_OAUTH_TOKEN')
+MY_ACCESS_TOKEN_SECRET = os.environ.get('EBOOKS_OAUTH_TOKEN_SECRET')
+
+# Sources (Twitter, Mastodon, local text file or a web page)
+SOURCE_EXCLUDE = r'^$'  # Source tweets that match this regexp will not be added to the Markov chain. You might want to filter out inappropriate words for example.
+STATIC_TEST = True # Set this to True if you want to test Markov generation from a static file instead of the API.
+TEST_SOURCE = os.environ.get('EBOOKS_TWITTER_USERNAME') + ".txt"  # The name of a text file of a string-ified list for testing. To avoid unnecessarily hitting Twitter API. You can use the included testcorpus.txt, if needed.
+
+ODDS = 8  # How often do you want this to run? 1/8 times?
+ORDER = 2  # How closely do you want this to hew to sensical? 2 is low and 4 is high.
+
+DEBUG = False  # Set this to False to start Tweeting live
+TWEET_ACCOUNT = os.environ.get('EBOOKS_TWITTER_USERNAME')  # The name of the account you're tweeting to.
+
