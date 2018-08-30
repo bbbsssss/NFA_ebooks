6c588134941bfe1cfaff04430cbd4f82194d881f
Bob Smith <rbrthsmth@gmail.com>
2018-08-30 11:46:16 -0400

    new env var for TEST_SOURCE

--- local_settings.py
+++ local_settings.py
@@ -14,7 +14,7 @@ MY_ACCESS_TOKEN_SECRET = os.environ.get('EBOOKS_OAUTH_TOKEN_SECRET')
 # Sources (Twitter, Mastodon, local text file or a web page)
 SOURCE_EXCLUDE = r'^$'  # Source tweets that match this regexp will not be added to the Markov chain. You might want to filter out inappropriate words for example.
 STATIC_TEST = True # Set this to True if you want to test Markov generation from a static file instead of the API.
-TEST_SOURCE = os.environ.get('EBOOKS_TWITTER_USERNAME') + ".txt"  # The name of a text file of a string-ified list for testing. To avoid unnecessarily hitting Twitter API. You can use the included testcorpus.txt, if needed.
+TEST_SOURCE = os.environ.get('EBOOKS_SOURCE_FILE') + ".txt"  # The name of a text file of a string-ified list for testing. To avoid unnecessarily hitting Twitter API. You can use the included testcorpus.txt, if needed.
 
 ODDS = 8  # How often do you want this to run? 1/8 times?
 ORDER = 2  # How closely do you want this to hew to sensical? 2 is low and 4 is high.
